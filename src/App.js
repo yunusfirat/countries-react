202bd464acb793576b27689e192791e22f38c00f
yunusfirat <yunusfirat44@gmail.com>
2021-05-04 01:55:24 +0100

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Countries from "./Countries";
+import Navbar from "./Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
      <Countries />
     </div>
   );
