ad286a07edc1e2170d5faae70d0ebc82832788c4
yunusfirat <yunusfirat44@gmail.com>
2021-05-04 01:35:12 +0100

    added countries card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Countries from "./Countries";
 function App() {
   return (
     <div className="App">
-     <h1>Hello world!</h1>
+     <Countries />
     </div>
   );
 }
---
+++ src/Countries.js
@@ -0,0 +1,30 @@
+import React from "react";
+import data from "./countriesAll.json";
+import "./styling/countries.css";
+console.log(data);
+const Countries = () => {
+
+
+
+
+return(
+    <div className="country-container">
+        {data.map((country,index) => {
+            const { name, population, flag, region, capital } = country;
+            return(
+                <div className="country" key={index}>
+                    <img src={flag} alt={name}/>
+                    <h5>{name}</h5>
+                    <strong>population:<span>{population}</span></strong>
+                    <strong>Region:<span>{region}</span></strong>
+                    <strong>Capital:<span>{capital}</span></strong>
+                </div>
+            );
+        })}
+        </div>
+);
+
+};
+
+
+export default Countries;
