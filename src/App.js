6d9a64fba1763e2f28343920f24cf8d364807142
yunusfirat <yunusfirat44@gmail.com>
2021-05-06 02:22:21 +0100

    dark mode implementation

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [regionInput, setRegionInput] = useState("");
-
+  const [clicked, SetClicked] = useState(false);
   return (
       <Router>
     <div className="App">
-      <Navbar />
+      <Navbar clicked={clicked} SetClicked={SetClicked} />
       <Switch>
         <Route exact path="/">
-      <Search searchInput={searchInput} regionInput={regionInput} setSearchInput={setSearchInput} setRegionInput={setRegionInput} />
-      <Countries  searchInput={searchInput}  regionInput={regionInput} />
+          <div className={clicked ? "darkmode" : ""}>
+      <Search searchInput={searchInput} regionInput={regionInput} setSearchInput={setSearchInput} setRegionInput={setRegionInput} clicked={clicked}/>
+      <Countries  searchInput={searchInput}  regionInput={regionInput} clicked={clicked} />
+          </div>
         </Route>
       <Route path="/country/:name" component={Country}/>
       </Switch>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./styling/navbar.css";
 
-const Navbar = () => {
-
+const Navbar = ({ clicked, SetClicked }) => {
+    const darkMode = () => {
+        SetClicked(!clicked);
+    };
 return (
-    <div className="navbar">
+    <div className={clicked ? "navbar-darkmode" : "navbar"}>
         <div>
-            <a className="linktitle" href="/"> Where in the world</a>
+            <a className={clicked ? "linktitle-darkmode" : "linktitle" } href="/"> Where in the world</a>
         </div>
         <div className="theme">
             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z"></path>
             </svg>
-            <p>Dark Mode</p>
+            <p onClick={darkMode}>Dark Mode</p>
         </div>
     </div>
 );
