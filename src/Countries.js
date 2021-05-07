c4d2c0d7fa8124a5f2f719d3cf15e536aa318bb5
yunusfirat <yunusfirat44@gmail.com>
2021-05-07 02:12:22 +0100

    Styling home page

--- src/Countries.js
+++ src/Countries.js
@@ -2,7 +2,7 @@ import React  from "react";
 import data from "./countriesAll.json";
 import "./styling/countries.css";
 import { Link } from "react-router-dom";
-const Countries = ( { searchInput, regionInput } ) => {
+const Countries = ( { searchInput, regionInput, clicked } ) => {
         function checkInput(country) {
             if(searchInput !==""){
                 return country.name.toLowerCase().includes(searchInput.toLowerCase())
@@ -18,12 +18,12 @@ return(
             const { name, population, flag, region, capital } = country;
                 return(
                     <Link to={`/country/${name}`} key={index} style={{ textDecoration:"none" }}>
-                    <div className="countries" key={index}>
+                    <div className={clicked ? "countries-darkmode" :"countries"} key={index} >
                         <img src={flag} alt={name}/>
-                        <h5>{name}</h5>
-                        <strong>population: <span>{population}</span></strong>
-                        <strong>Region: <span>{region}</span></strong>
-                        <strong>Capital: <span>{capital}</span></strong>
+                        <h5 className = {clicked ? "h5-darkmode" : null}>{name}</h5>
+                        <strong className={clicked ? "strong-darkmode" : null}>population: <span className={clicked ? "span-darkmode" : null}>{population}</span></strong>
+                        <strong className={clicked ? "strong-darkmode" : null}>Region: <span className={clicked ? "span-darkmode" : null}>{region}</span></strong>
+                        <strong className={clicked ? "strong-darkmode" : null}>Capital: <span className={clicked ? "span-darkmode" : null}>{capital}</span></strong>
                     </div>
                     </Link>
                 );
